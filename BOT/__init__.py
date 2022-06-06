b23680588e316be479d9195b259e4bc74f14447b
amirali rajabi <75732031+amiralirj@users.noreply.github.com>
2022-06-06 15:01:31 +0430

    Add files via upload

---
+++ BOT/__init__.py
@@ -0,0 +1,12 @@
+﻿import sys
+import os
+SCRIPT_DIR = str(os.path.dirname(os.path.abspath(__file__))).strip(r'\plugins\BOT')
+sys.path.insert(-1,(SCRIPT_DIR))
+CWD=os.getcwd()
+del os , sys
+from Advertiserbot import Advertising , Rj
+import  Config
+from Config import TEXTS , BUTTONS 
+from Classes.NumberClass import Number
+from Classes.UserClass import User
+RJ=Rj()
